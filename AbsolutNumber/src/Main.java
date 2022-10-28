ee60a9d3c03fce009c925e99b8b1cd352bcdc3a5
IdanZakhaim <92606616+IdanZakhaim@users.noreply.github.com>
2022-10-28 10:55:59 +0300

    Add files via upload

---
+++ AbsolutNumber/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ("Enter a number:  ");
+        int userInput = scanner.nextInt();
+        if (userInput < 0) {
+            System.out.println ("Your Absolut number is:   "  + - userInput);
+        }
+        else  {
+            System.out.println("Your Absolut number is:" + userInput);
+        }
+        System.out.println("Good bye");
+    }
+}
